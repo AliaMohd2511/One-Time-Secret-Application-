6603798a02f0f0e3e940f602ccb1a990a8f8607d
AliaMohd2511 <160072213+AliaMohd2511@users.noreply.github.com>
2025-02-06 12:00:33 +0400

    Create app.component.ts

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+main { 
+  font-family: Arial, sans-serif;
+  background-color: #f0f0f0;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.brand-name {
+  background-color: #f8f8f800;
+  display: flex;
+  align-items: center;
+  padding: 2rem;
+}
+
+.brand-logo {
+  width: 60px;
+  margin-left: 25px; /* Adjust this value as needed */
+}
+
+.content {
+  flex: 1;
+  padding: 20px;
+}
